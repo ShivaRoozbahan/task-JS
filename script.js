f2c9e2c3c2d716b3a1e38ec2ba01a0fb55919b97
Shiva <shivaroozbahani@gmail.com>
2024-09-07 17:30:46 +0330

    area and perimete

---
+++ script.js
@@ -0,0 +1,19 @@
+const lenght = 10;
+const width = 5;
+const radius = 7;
+const height = 15;
+const base1 = 8;
+const base2 = 18;
+const leg = 15;
+
+//  rectangle
+console.log("area rectangl= " + lenght * width);
+console.log("perimete rectangl= " + (lenght + width) * 2);
+
+//circle
+console.log("perimete circle= " + radius * radius * 3.14);
+console.log("area circle= " + (radius + radius) * 3.14);
+
+//trapezoid
+console.log("perimete trapezoid= " + (base1 + base2) * height * 2);
+console.log("area trapezoid= " + base1 + base2 + leg * 2);
